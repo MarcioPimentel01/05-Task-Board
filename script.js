f5f93c9da08aa5da8a95b26b92d874b7e8afc8fb
Marcio Pimentel <149727150+MarcioPimentel01@users.noreply.github.com>
2024-05-02 06:26:12 -0400

    card element create into the column

--- script.js
+++ script.js
@@ -1,35 +1,63 @@
-const myModal = document.getElementById('myModal')
-const myInput = document.getElementById('myInput')
-const btnSaveTask = document.querySelector(`.btn-primary`)
+document.addEventListener(`DOMContentLoaded`, function() {
+    
+    const myModal = document.getElementById('myModal')
+    const myInput = document.getElementById('myInput')
+    const btnSaveTask = document.getElementById(`btn-close`)
+    const _createCardTask = document.getElementById(`create-task-card`)
+    
+    
+    objTask = []
+    
+    
+    btnSaveTask.addEventListener(`click`, () => {
+        console.log(`ok`)
+        getTask()
+    })
+    
+    function getTask () {
+        const taskTitle = document.querySelector('#exampleModal input[name="task-title"]').value;
+        const taskDate = document.querySelector('#exampleModal input[type="date"]').value;
+        const taskDescription = document.querySelector('#exampleModal textarea').value;
+        
+        const task = {
+            title: taskTitle,
+            date: taskDate,
+            description: taskDescription
+        };
+        
+        // Normally, the order would be to use setItem. However, the line below parses the 'objTask' object from localStorage and initializes an empty array if no tasks exist.
+        // In other words, this line parses the stored tasks from localStorage into JSON format so they can later be added to the 'objTask' array. Only after that, the tasks are stringified and set into localStorage.
+        const objTask = JSON.parse(localStorage.getItem('tasks')) || [];
 
+        objTask.push(task);
+        
+        const objTaskJson = JSON.stringify(objTask);
+        
+        localStorage.setItem('tasks', objTaskJson);
+        createCards()
+    }
+    
+    function createCards () {
+        const tasks = JSON.parse(localStorage.getItem('tasks')) || []; // Retrieve tasks from localStorage and parse as JSON
+        console.log(tasks);
 
-addedTasks = []
-
-
-btnSaveTask.addEventListener(`click`, () => {
-    console.log(`ok`)
-    getTask()
+        tasks.forEach(task => {
+            const createTaskCard = document.createElement(`div`)
+            createTaskCard.classList.add(`task`)
+            
+            createTaskCard.innerHTML = `
+            <div class="card text-bg-primary mb-3" id="draggableDiv" draggable="true" style="max-width: 22rem;">
+                <div class="card-header">${task.title}</div>
+                    <div class="card-body">
+                    <h5 class="card-title">${task.title}</h5>
+                        <p class="card-text">${task.description}</p>
+                    <div class="btn-card"><button type="button" class="btn btn-light">Delete Task</button></div>
+                </div>
+            </div>`
+            _createCardTask.appendChild(createTaskCard)
+        });
+        
+    }
 })
-
-function getTask () {
-    const taskTitle = document.querySelector('#exampleModal input[name="task-title"]').value;
-    const taskDate = document.querySelector('#exampleModal input[type="date"]').value;
-    const taskDescription = document.querySelector('#exampleModal textarea').value;
-    
-    const task = {
-        title: taskTitle,
-        date: taskDate,
-        description: taskDescription
-    };
-    
-    const addedTasks = JSON.parse(localStorage.getItem('tasks')) || [];
-    
-    addedTasks.push(task);
-    
-    const tasksJson = JSON.stringify(addedTasks);
-    
-    localStorage.setItem('tasks', tasksJson);
-    
-}
 
 
