b748165a4d1d86736f73cab61d18dcee06a1aace
Marcio Pimentel <149727150+MarcioPimentel01@users.noreply.github.com>
2024-05-12 18:28:00 -0400

    last searches menu added

--- script.js
+++ script.js
@@ -1,18 +1,40 @@
 document.addEventListener(`DOMContentLoaded`, function() {
     
-    const myModal = document.getElementById('myModal')
-    const myInput = document.getElementById('myInput')
-    const btnSaveTask = document.getElementById(`btn-close`)
-    const _createCardTask = document.getElementById(`create-task-card`)
+    // const myModal = document.getElementById('myModal')
+    // const myInput = document.getElementById('myInput')
+    const btnSaveTask = document.getElementById(`btn-close`);
+    const _createCardTask = document.querySelectorAll(`.create-task-card`);
+    const _draggableDiv = document.querySelectorAll (`.draggableDiv`);
+    const _dropDraggableDiv = document.querySelectorAll(`.main-columns`);
     
     
-    objTask = []
+    objTask = [];
     
     
     btnSaveTask.addEventListener(`click`, () => {
         console.log(`ok`)
         getTask()
-    })
+    });
+
+    _draggableDiv.forEach(draggable => {
+        draggable.addEventListener('dragstart', (event) => {
+            event.dataTransfer.setData('text/plain', draggable.id); 
+        });
+    });
+    
+    _dropDraggableDiv.forEach(dropArea => {
+        dropArea.addEventListener('dragover', (event) => {
+            event.preventDefault(); 
+        });
+        
+        dropArea.addEventListener('drop', (event) => {
+            event.preventDefault();
+            const draggableId = event.dataTransfer.getData('text/plain');
+            const draggableElement = document.getElementById(draggableId);
+            dropArea.appendChild(draggableElement); 
+        });
+    });
+    
     
     function getTask () {
         const taskTitle = document.querySelector('#exampleModal input[name="task-title"]').value;
@@ -34,27 +56,31 @@ document.addEventListener(`DOMContentLoaded`, function() {
         const objTaskJson = JSON.stringify(objTask);
         
         localStorage.setItem('tasks', objTaskJson);
+
         createCards()
     }
     
     function createCards () {
+
+
         const tasks = JSON.parse(localStorage.getItem('tasks')) || []; // Retrieve tasks from localStorage and parse as JSON
         console.log(tasks);
 
         tasks.forEach(task => {
-            const createTaskCard = document.createElement(`div`)
-            createTaskCard.classList.add(`task`)
+            const createTaskCard = document.createElement(`div`);
+            createTaskCard.classList.add(`task`);
             
             createTaskCard.innerHTML = `
-            <div class="card text-bg-primary mb-3" id="draggableDiv" draggable="true" style="max-width: 22rem;">
-                <div class="card-header">${task.title}</div>
+            <div class="card text-bg-primary mb-3 draggableDiv" draggable="true" style="max-width: 22rem;">
+                <div class="card-header">${task.date}</div>
                     <div class="card-body">
                     <h5 class="card-title">${task.title}</h5>
                         <p class="card-text">${task.description}</p>
                     <div class="btn-card"><button type="button" class="btn btn-light">Delete Task</button></div>
                 </div>
             </div>`
-            _createCardTask.appendChild(createTaskCard)
+
+            _createCardTask[0].appendChild(createTaskCard);
         });
         
     }
